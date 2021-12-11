997479be49f4c86317c150bc23e13a5a9f43eb57
ExpressGradient <johndoeknown@protonmail.com>
2021-12-11 13:01:54 +0530

    added Visualizer and Download Dataset

--- pages/dashboard/labs/[labSlug]/models/[modelSlug].js
+++ pages/dashboard/labs/[labSlug]/models/[modelSlug].js
@@ -34,13 +34,27 @@ import {
     Code,
     useBoolean,
     Select,
+    Flex,
+    Link,
 } from "@chakra-ui/react";
-import { useRef, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useEffect, useRef, useState } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
 import useSWR, { useSWRConfig } from "swr";
 import { useRouter } from "next/router";
 import { ViewOffIcon, ViewIcon } from "@chakra-ui/icons";
 import Script from "next/script";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    Tooltip,
+    YAxis,
+    CartesianGrid,
+    Legend,
+    BarChart,
+    Bar,
+} from "recharts";
+import { TwitterPicker } from "react-color";
 
 const AddDataFormControl = ({ field, type, register }) => {
     switch (type) {
@@ -109,6 +123,9 @@ const Explorer = ({ model, SSRrows }) => {
 
     return (
         <VStack direction="column">
+            <Button colorScheme="teal" ref={buttonRef} onClick={onOpen}>
+                Add Data Points
+            </Button>
             <Table variant="striped">
                 <Thead>
                     <Tr>
@@ -121,18 +138,13 @@ const Explorer = ({ model, SSRrows }) => {
                     {rows &&
                         rows.map((row) => (
                             <Tr key={row.data.id}>
-                                {Object.entries(row.data).map(
-                                    ([field, value]) => (
-                                        <Td key={value}>{value}</Td>
-                                    )
-                                )}
+                                {Object.entries(row.data).map(([_, value]) => (
+                                    <Td key={value}>{value}</Td>
+                                ))}
                             </Tr>
                         ))}
                 </Tbody>
             </Table>
-            <Button colorScheme="teal" ref={buttonRef} onClick={onOpen}>
-                Add Data Points
-            </Button>
             <Drawer
                 isOpen={isOpen}
                 placement="right"
@@ -193,10 +205,25 @@ const Explorer = ({ model, SSRrows }) => {
     );
 };
 
-const Developer = ({ apiKey }) => {
+const Developer = ({ apiKey, rows, properties }) => {
     const router = useRouter();
     const { modelSlug } = router.query;
     const [viewAble, setViewAble] = useBoolean(false);
+    const [encodedUri, setEncodedUri] = useState("");
+
+    useEffect(() => {
+        let csvContent = "data:text/csv;charset=utf-8,";
+        csvContent += Object.keys(properties).join(",") + "\n";
+
+        const transformedRows = rows.map((row) => row.data);
+
+        transformedRows.forEach((row) => {
+            csvContent += Object.values(row).join(",") + "\n";
+        });
+
+        const encodedUri = encodeURI(csvContent);
+        setEncodedUri(encodedUri);
+    }, []);
 
     return (
         <Stack direction="column">
@@ -222,7 +249,182 @@ const Developer = ({ apiKey }) => {
                     )}
                 </Stack>
             </Heading>
+            <Heading size="md" as="u">
+                <Link
+                    color="blue.400"
+                    href={encodedUri}
+                    download={`${modelSlug}_data.csv`}
+                >
+                    Download Dataset
+                </Link>
+            </Heading>
         </Stack>
+    );
+};
+
+const Visualizer = ({ properties, rows }) => {
+    const {
+        register,
+        control,
+        watch,
+        formState: { isSubmitting },
+        handleSubmit,
+    } = useForm();
+    const { fields, append } = useFieldArray({ control, name: "lines" });
+    const [strokes, setStrokes] = useState([]);
+    const [showChart, setShowChart] = useState(false);
+
+    const appendHandler = () => {
+        append({ line: "" });
+        setStrokes((prevStrokes) => [...prevStrokes, "#FF6900"]);
+    };
+
+    const onStrokeChange = (color, index) => {
+        setStrokes((prevStrokes) => {
+            prevStrokes[index] = color.hex;
+            return [...prevStrokes];
+        });
+    };
+
+    const onSubmitHandler = () => setShowChart(true);
+
+    const transformedRows = rows.map((row) => row.data);
+
+    const onReset = () => setShowChart(false);
+
+    return (
+        <>
+            {showChart ? (
+                <VStack>
+                    {watch("chartType") === "lineChart" ? (
+                        <LineChart
+                            width={700}
+                            height={300}
+                            data={transformedRows}
+                        >
+                            <XAxis dataKey={watch("xAxis")} />
+                            <YAxis />
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <Tooltip />
+                            <Legend />
+                            {watch("lines").map((line, index) => (
+                                <Line
+                                    key={index}
+                                    dataKey={line.dataKey}
+                                    stroke={strokes[index]}
+                                />
+                            ))}
+                        </LineChart>
+                    ) : (
+                        <BarChart
+                            width={700}
+                            height={300}
+                            data={transformedRows}
+                        >
+                            <XAxis dataKey={watch("xAxis")} />
+                            <YAxis />
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <Tooltip />
+                            <Legend />
+                            {watch("lines").map((line, index) => (
+                                <Bar
+                                    key={index}
+                                    dataKey={line.dataKey}
+                                    fill={strokes[index]}
+                                />
+                            ))}
+                        </BarChart>
+                    )}
+                    <Button onClick={onReset}>Reset</Button>
+                </VStack>
+            ) : (
+                <Stack
+                    direction="column"
+                    spacing={4}
+                    as="form"
+                    w={["100%", 1 / 3]}
+                    mx={[4, "auto"]}
+                    onSubmit={handleSubmit(onSubmitHandler)}
+                >
+                    <FormControl isRequired>
+                        <FormLabel htmlFor="chart-type">Chart Type:</FormLabel>
+                        <Select
+                            variant="filled"
+                            id="chart-type"
+                            {...register("chartType")}
+                        >
+                            <option value="lineChart">Line Chart</option>
+                            <option value="barChart">Bar Chart</option>
+                        </Select>
+                    </FormControl>
+
+                    <FormControl isRequired>
+                        <FormLabel htmlFor="x-axis">X Axis:</FormLabel>
+                        <Select
+                            variant="filled"
+                            id="x-axis"
+                            {...register("xAxis")}
+                        >
+                            {Object.keys(properties).map((field) => (
+                                <option key={field}>{field}</option>
+                            ))}
+                        </Select>
+                    </FormControl>
+
+                    <FormControl isRequired>
+                        <FormLabel>
+                            Add{" "}
+                            {watch("chartType") === "lineChart"
+                                ? "Lines"
+                                : "Bars"}
+                        </FormLabel>
+
+                        <Button onClick={appendHandler} mb={4}>
+                            Add{" "}
+                            {watch("chartType") === "lineChart"
+                                ? "Line"
+                                : "Bar"}
+                        </Button>
+
+                        <Flex direction="column" mx={0}>
+                            {fields.map((field, index) => (
+                                <Flex direction="column" key={field.id} mb={4}>
+                                    <Select
+                                        variant="filled"
+                                        {...register(`lines.${index}.dataKey`)}
+                                        mb={4}
+                                    >
+                                        {Object.keys(properties).map(
+                                            (field) => (
+                                                <option key={field}>
+                                                    {field}
+                                                </option>
+                                            )
+                                        )}
+                                    </Select>
+
+                                    <TwitterPicker
+                                        color={strokes[index]}
+                                        onChangeComplete={(color) =>
+                                            onStrokeChange(color, index)
+                                        }
+                                    />
+                                </Flex>
+                            ))}
+                        </Flex>
+                    </FormControl>
+
+                    <Button
+                        type="submit"
+                        colorScheme="teal"
+                        variant="solid"
+                        isLoading={isSubmitting}
+                    >
+                        Create
+                    </Button>
+                </Stack>
+            )}
+        </>
     );
 };
 
@@ -456,8 +658,8 @@ const ModelSlug = ({ payload }) => {
                         <Tab>Developer</Tab>
                         <Tab>
                             <HStack>
-                                <Text>Charts</Text>
-                                <Badge colorScheme="blue">Alpha</Badge>
+                                <Text>Visualizer</Text>
+                                <Badge colorScheme="orange">Beta</Badge>
                             </HStack>
                         </Tab>
                         <Tab>
@@ -476,9 +678,18 @@ const ModelSlug = ({ payload }) => {
                             />
                         </TabPanel>
                         <TabPanel>
-                            <Developer apiKey={payload.model.key} />
+                            <Developer
+                                apiKey={payload.model.key}
+                                properties={payload.model.schema.properties}
+                                rows={payload.rows}
+                            />
                         </TabPanel>
-                        <TabPanel></TabPanel>
+                        <TabPanel>
+                            <Visualizer
+                                properties={payload.model.schema.properties}
+                                rows={payload.rows}
+                            />
+                        </TabPanel>
                         <TabPanel>
                             <MachineLearning
                                 properties={payload.model.schema.properties}
