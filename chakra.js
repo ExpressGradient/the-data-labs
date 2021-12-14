88313943d3392c73058e4cfcd6b8cf278bb1fc89
Free Guy <48156992+SiddamreddyPavankumarReddy@users.noreply.github.com>
2021-12-14 21:36:32 +0530

    Add files via upload

---
+++ chakra.js
@@ -0,0 +1,17 @@
+// store input numbers
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
